2f4ee87e05141a8ac7382cd36724b6c3b9a86f20
Nikita Shapoval <n.shapoval@dunice.net>
2018-09-28 16:22:19 +0300

    Rewrote with ref's

--- src/pages/profile/components/profileForm.jsx
+++ src/pages/profile/components/profileForm.jsx
@@ -1,16 +1,18 @@
-import { Form } from 'semantic-ui-react';
+import { Form, Label } from 'semantic-ui-react';
 import React, { PureComponent } from 'react';
 import actions from '../actions';
 import {connect} from 'react-redux';
 import {selectProfile} from '../selectors';
-import { stat } from 'fs';
 
 class ProfileForm extends PureComponent {
   constructor(props){
     super(props);
-    this.state = {
-      user: {},
-    }
+    this.firstName = React.createRef();
+    this.lastName = React.createRef();
+    this.company = React.createRef();
+    this.department = React.createRef();
+    this.position = React.createRef();
+    this.email = React.createRef();
   }
 
   componentDidMount(){
@@ -23,34 +25,86 @@ class ProfileForm extends PureComponent {
 
   handleSubmit = () => {
     const user = {
-      ...this.props.user,
-      ...this.state.user,
+      firstName: this.firstName.current.value.trim(),
+      lastName: this.lastName.current.value.trim(),
+      company: this.company.current.value.trim(),
+      department: this.department.current.value.trim(),
+      position: this.position.current.value.trim(),
+      email: this.email.current.value.trim(),
     }
     this.props.saveUser(user);
   }
 
-  handleChange = (e, { name, value }) => this.setState({ 
-    user: {
-      ...this.state.user,
-      [name]: value.trim() ? value : '',
-    } 
-  })
+  handleChange = ({ name, value }) => {
+
+  }
+
 
   render() {
-    const {isLoading, user: propsUser} = this.props;
-    const {user: stateUser} = this.state;
-    const user = {
-      ...propsUser,
-      ...stateUser,
-    }
+    const {isLoading, user} = this.props;
       return (
         <Form loading={isLoading} className = "profileForm" onSubmit={this.handleSubmit}> 
-          <Form.Input inline label="First Name" name='firstName' value={user.firstName} onChange={this.handleChange} />
-          <Form.Input inline label="Last Name" name='lastName' value={user.lastName} onChange={this.handleChange} />
-          <Form.Input inline label="Company" name='company' value={user.company} onChange={this.handleChange} />
-          <Form.Input inline label="Department" name='department' value={user.department} onChange={this.handleChange} />
-          <Form.Input inline label="Position" name='position' value={user.position} onChange={this.handleChange} />
-          <Form.Input inline label="Email" name='email' value={user.email} onChange={this.handleChange} />
+          <Form.Field inline >
+            <Label>
+              First Name
+            </Label>
+            <input
+              name='firstName'
+              ref={this.firstName}
+              defaultValue={user.firstName} 
+              onChange={this.handleChange} />
+          </Form.Field>
+          <Form.Field inline >
+            <Label>
+              Last Name
+            </Label>
+            <input
+              name='lastName'
+              ref={this.lastName}
+              defaultValue={user.lastName} 
+              onChange={this.handleChange} />
+          </Form.Field>
+          <Form.Field inline >
+            <Label>
+              Company
+            </Label>
+            <input
+              name='company'
+              ref={this.company}
+              defaultValue={user.company} 
+              onChange={this.handleChange} />
+          </Form.Field>
+          <Form.Field inline >
+            <Label>
+              Department
+            </Label>
+            <input
+              name='department'
+              ref={this.department}
+              defaultValue={user.department} 
+              onChange={this.handleChange} />
+          </Form.Field>
+          <Form.Field inline >
+            <Label>
+              Position
+            </Label>
+            <input
+              name='position'
+              ref={this.position}
+              defaultValue={user.position} 
+              onChange={this.handleChange} />
+          </Form.Field>
+          <Form.Field inline >
+            <Label>
+              Email
+            </Label>
+            <input
+              name='email'
+              type="email"
+              ref={this.email}
+              defaultValue={user.email} 
+              onChange={this.handleChange} />
+          </Form.Field>
           <Form.Button content='Save' />
         </Form>
       );
