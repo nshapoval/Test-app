f487b56dd4d4d2af94bcf3886d935eb3864aae37
mac <e.holodov@dunice.net>
2018-09-28 18:12:39 +0300

    fixed

--- src/pages/profile/container.jsx
+++ src/pages/profile/container.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Body from './body';
-import { Header } from '../../components/Header/index';
-import { Footer } from '../../components/Footer/index';
+import { Header } from '../../components/Header';
+import { Footer } from '../../components/Footer';
 import './style.scss';
 
 const HomePage = () => (
